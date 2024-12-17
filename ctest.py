09c3ecdee7d34a268edf1032311ee9ea7ed8dc65
maglore9900 <78699112+maglore9900@users.noreply.github.com>
2024-12-17 16:21:18 -0500

    Update ctest.py

--- ctest.py
+++ ctest.py
@@ -8,7 +8,8 @@ environ.Env.read_env()
 ad = adapter.Adapter(env)
 ag = agent.Agents(env)
 
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 print(loop.run_until_complete(ag.invoke_agent("tell me about scp-5000", "Smith")))
 
 # print(ad.chat("tell me about yourself", "Smith"))
